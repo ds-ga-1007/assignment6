642c6e2e862010d229185cc61e1ba96fb6ffe857
Han Zhao <hz1411@nyu.edu>
2016-11-02 17:08:44 -0400

    add file

---
+++ my_package/assignment6.py
@@ -0,0 +1,15 @@
+'''
+Created on Nov 2, 2016
+
+@author: evelynz
+
+'''
+
+def hello_world():
+    print "Hello World: ", time.strftime("%c")
+#
+# Python hello world
+#
+if __name__ == "__main__":
+    import sys, time
+    hello_world()
