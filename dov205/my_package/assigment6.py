cb2ef6f214ef28806d722e64c71e1e0de0381a7a
Danny Vilela <dov205@nyu.edu>
2016-11-02 23:37:50 -0400

    my_package (assignment6.py), answers.txt

---
+++ dov205/my_package/assigment6.py
@@ -0,0 +1,14 @@
+'''
+@author: Danny Vilela
+'''
+
+def hello_world():
+    print("Hello World: ", time.strftime("%c"))
+
+#
+# Python hello world
+#
+if __name__ == "__main__":
+    import sys, time
+    hello_world()
+
