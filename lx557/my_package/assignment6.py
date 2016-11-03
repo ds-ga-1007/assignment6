5f8ce7febe3232d85ad80a83c81e238133f089c1
xlxulei1005 <lx557@nyu.edu>
2016-11-02 22:49:23 -0400

    Assignment6_lx557

---
+++ lx557/my_package/assignment6.py
@@ -0,0 +1,15 @@
+'''
+Created on 2016.11.2
+
+@author: xulei
+'''
+def hello_world():
+    return print("Hello World: ", time.strftime("%c"))
+
+
+#
+# Python hello world
+#
+if __name__ == "__main__":
+    import sys, time
+    hello_world()
