48e0ec1b26e2b9f4fb2d449b8dc759a2aad6c0a3
xy990 <xy990@nyu.edu>
2016-11-03 17:35:14 -0400

    assignment6

---
+++ xy990/my_package/assignment6.py
@@ -0,0 +1,18 @@
+'''
+Created on 2016/11/03
+
+@author: sherryyang
+
+'''
+def hello_world():
+    print("Hello World: ", time.strftime("%c"))
+
+
+
+#
+# Python hello world
+#
+if __name__ == "__main__":
+    import sys, time
+    hello_world()
+
