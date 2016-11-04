07635232d3283370aaf117c9d8f6fb9ab72dd7b3
wz1070 <wz1070@nyu.edu>
2016-11-04 15:24:46 -0400

    wz1070 assignment6

---
+++ wz1070/my_package/assignment6.py
@@ -0,0 +1,15 @@
+'''
+Created on Nov 4, 2016
+
+@author: WenqingZhu
+'''
+
+def hello_world():
+    print "Hello World: ", time.strftime("%c")
+
+#
+# Python hello world
+#
+if __name__ == "__main__":
+    import sys, time
+    hello_world()
