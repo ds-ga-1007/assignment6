9c23193d9fd89613966c3d76e17cbd4b7c9300c4
Yaohan Ke <yk1587@nyu.edu>
2016-11-03 20:01:17 -0400

    assignment6 yk1587

---
+++ yk1587/my_package/assignment6.py
@@ -0,0 +1,16 @@
+'''
+Created on 2016年11月3日
+
+@author: keyaohan
+
+
+'''
+def hello_world():
+    return print("Hello World: ", time.strftime("%c"))
+
+#
+# Python hello world
+#
+if __name__ == "__main__":
+    import sys, time
+    hello_world()
